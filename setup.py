aaa0185bacc0cb6d6e2acab0c9244d9ead7e4767
Gamaliel Espinoza <gamaliel.espinoza@gmail.com>
2017-09-09 21:38:19 -0700

    Version in setup is taken from the pymago.__version__.

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
+from __future__ import absolute_import
 from os.path import join, dirname
 from setuptools import setup
+import pymago
 
 basepath = dirname(__file__)
 binpath = join(basepath, 'bin')
@@ -7,7 +9,7 @@ binpath = join(basepath, 'bin')
 setup(
   name = 'pymago',
   packages = ['pymago'],
-  version = '0.1a4',
+  version = pymago.__version__,
   description = 'CLI Tool for optimizing folder of images',
   long_description = open(join(basepath, 'README.txt')).read(),
   scripts = [join(binpath, 'pymago')],
